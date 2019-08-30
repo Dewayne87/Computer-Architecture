2c7e04c683ef5e32401338c0be7eee7d7b6be4a4
Dewayne87 <dewaynelindsay87@gmail.com>
2019-08-29 18:47:16 -0700

    day 1 and 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,33 +2,79 @@
 
 import sys
 
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+MUL = 0b10100010
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.pc = 0
+        self.hlt = False
 
-    def load(self):
+        self.ops ={
+            LDI: self.op_ldi,
+            HLT: self.op_hlt,
+            MUL: self.op_mul,
+            PRN: self.op_prn
+        }
+    
+    def ram_read(self,mar):
+        return self.ram[mar]
+
+    def ram_write(self,mar,mdr):    
+        self.ram[mar] = mdr
+
+
+    def op_ldi(self,addr,val):
+        self.reg[addr] = val
+
+    def op_mul(self,addr1,addr2):
+        self.alu("MUL",addr1,addr2)
+
+    def op_hlt(self,op_a,op_b):
+        self.hlt = True
+    
+    def op_prn(self,addr,op_b):
+        print(self.reg[addr])
+
+    def load(self,filename):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+        with open(filename) as file:
+            for line in file:
+                comment_split = line.split('#')
+                instruction = comment_split[0]
+
+                if instruction == '':
+                    continue
+                first_bit = instruction[0]
+                if first_bit == '0' or first_bit == '1':
+                    self.ram[address] = int(instruction[:8],2)
+                    address += 1
+
 
 
     def alu(self, op, reg_a, reg_b):
@@ -37,6 +83,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -62,4 +110,18 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        # LDI = 0b10000010
+        # PRN = 0b01000111
+        # HLT = 0b00000001
+
+        while not self.hlt:
+            ir = self.ram_read(self.pc)
+            op_a = self.ram_read(self.pc + 1)
+            op_b = self.ram_read(self.pc + 2)
+            op_size = ir >> 6
+            ins_set = ((ir >> 4) & 0b1) == 1
+            if ir in self.ops:
+                self.ops[ir](op_a,op_b)
+            if not ins_set:
+                self.pc += op_size + 1
+
