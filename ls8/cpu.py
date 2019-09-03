4266585e5a1c9aea5b555c94149989c1146bb68a
Dewayne87 <dewaynelindsay87@gmail.com>
2019-09-03 05:32:47 -0700

    sprint attempt

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -6,6 +6,11 @@ LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
 MUL = 0b10100010
+CMP = 0b10000010
+JMP = 0b01000111
+JEQ = 0b00000001
+JNE = 0b10100010
+RET = 0b00010001
 
 class CPU:
     """Main CPU class."""
@@ -15,13 +20,21 @@ class CPU:
         self.reg = [0] * 8
         self.ram = [0] * 256
         self.pc = 0
+        self.FL = 0b00000000
         self.hlt = False
 
         self.ops ={
             LDI: self.op_ldi,
             HLT: self.op_hlt,
             MUL: self.op_mul,
-            PRN: self.op_prn
+            PRN: self.op_prn,
+            JMP: self.op_jmp,
+            CMP: self.op_cmp,
+            JEQ: self.op_jeq,
+            JNE: self.op_jne,
+            RET: self.op_ret
+
+
         }
     
     def ram_read(self,mar):
@@ -42,6 +55,30 @@ class CPU:
     
     def op_prn(self,addr,op_b):
         print(self.reg[addr])
+    
+    def op_jne(self,op_a,op_b):
+        flag = (self.FL & 0b1) == 1
+        if not flag:
+            self.op_jmp(op_a,op_b)
+
+    def op_cmp(self,addr1,addr2):
+        self.alu("CMP",addr1,addr2)
+
+    def op_jmp(self,op_a,op_b):
+        register_address = self.ram[op_a]
+        address_to_jump_to = self.reg[register_address]
+        self.pc = address_to_jump_to
+
+    def op_ret(self,op_a,op_b):
+        SP = self.reg[7]
+        address_to_return_to = self.ram[SP]
+        self.reg[7] = (SP + 1) % 243
+
+    def op_jeq(self,op_a,op_b):
+        flag = (self.FL & 0b1) == 1
+        if flag:
+            self.op_jmp(op_a,op_b)
+
 
     def load(self,filename):
         """Load a program into memory."""
@@ -79,12 +116,20 @@ class CPU:
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
+        print(self.reg[reg_a],"here")
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "CMP":
+            if self.reg[reg_a] > self.reg[reg_b]:
+                print(self.reg[reg_a])
+                self.FL = 0b00000010
+            if self.reg[reg_a] == self.reg[reg_b]:
+                self.FL = 0b00000001
+            if self.reg[reg_a] < self.reg[reg_b]:
+                self.FL = 0b00000100
         else:
             raise Exception("Unsupported ALU operation")
 
